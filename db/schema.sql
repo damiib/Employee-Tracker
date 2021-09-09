1c4872b293ca1382a48c340dbfb5615b100fcde4
Damali <brown@MacBook-Pro.local>
2021-09-09 14:05:44 -0700

    beginning the tracker assignment

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+// department table needs department names and ids
+//role table needs job title, role id, department and salary
+// employee table needs employee ids, first name, last name, job title, department, salaries and their manager
