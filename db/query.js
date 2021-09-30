20f321e78acb3aa6270e31855dbc3663e743d3f2
Damali <brown@MacBook-Pro.local>
2021-09-29 17:41:53 -0700

    added the add department function and the insert employees

--- db/query.js
+++ db/query.js
@@ -1,48 +1,70 @@
 // Import and require mysql2
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // Connect to database
 const connection = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // TODO: Add MySQL password here
-      password: '13130000',
-      database: 'employeetracker_db'
-    },
-    console.log(`Connected to the movies_db database.`)
-  );
-  
-  connection.connect(function(err){
-      if (err) throw err
-  })
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // TODO: Add MySQL password here
+    password: "13130000",
+    database: "employeetracker_db",
+  },
+  console.log(`Connected to the movies_db database.`)
+);
 
-  class DB {
-      constructor(connection){
-          this.connection = connection
-      }
+connection.connect(function (err) {
+  if (err) throw err;
+});
 
-      //findAllEmployee() function goes here. We need it to run a query with a select statement.
-
-      findAllEmployees(){
-        return this.connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id LEFT JOIN employee manager ON manager.id = employee.manager_id;")
-      }
-
-      findAllDepartments(){
-        return this.connection.promise().query("SELECT * FROM department;")
-      }
-    //   SELECT role.id, role.salary, CONCAT(employee.first_name, " ", employee.last_name) AS employeename FROM employee LEFT JOIN role ON role.id = employee.role_id;
-
-    //(select *) = all the columns and the data associated with them.
-    //(FROM 'tableName') = the table where the data originates from.
-      
-
-    //create a function called findAllRoles. this select statement needs to brings in the columns role id, role title, the department name, and the role salary. you need to Left Join the department table. Test in mysql CLI.
-    findAllRoles(){
-      return this.connection.promise().query("SELECT role.id,role.title,department.name, role.salary FROM role LEFT JOIN department ON role.department_id = department.id;") 
-    }
-    
+class DB {
+  constructor(connection) {
+    this.connection = connection;
   }
 
-  module.exports = new DB(connection);
+  //findAllEmployee() function goes here. We need it to run a query with a select statement.
+
+  findAllEmployees() {
+    return this.connection
+      .promise()
+      .query(
+        "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
+      );
+  }
+
+  findAllDepartments() {
+    return this.connection.promise().query("SELECT * FROM department;");
+  }
+  
+
+  //(select *) = all the columns and the data associated with them.
+  //(FROM 'tableName') = the table where the data originates from.
+
+  //create a function called findAllRoles. this select statement needs to brings in the columns role id, role title, the department name, and the role salary. you need to Left Join the department table. Test in mysql CLI.
+  findAllRoles() {
+    return this.connection
+      .promise()
+      .query(
+        "SELECT role.id,role.title,department.name, role.salary FROM role LEFT JOIN department ON role.department_id = department.id;"
+      );
+  }
+  //update all roles
+
+  updateEmployeesRoles(empId, roleId){
+    return this.connection
+    .promise()
+    .query("UPDATE employee SET role_id= ? WHERE id = ?", 
+    [roleId, empId]
+    )
+  }
+
+  addDepartment(department){
+    return this.connection
+    .promise()
+    .query("INSERT INTO department SET ?", department)
+  }
+  
+}
+
+module.exports = new DB(connection);
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require("console.table");
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const { updateEmployeesRoles } = require("./db/query.js");
 const db = require("./db/query.js");
 
 const questions = [
@@ -35,11 +36,14 @@ function start() {
       case "View All Roles":
         viewAllRoles();
         break;
+      case "Update Employee Role":
+        updateEmployeeRole();
+        break;
 
+case "Add Departments":
+  addDepartments()
+  break;
 
-
-
-        
       default:
           process.exit();
     }
@@ -67,6 +71,56 @@ function viewAllDepartments() {
         start();
     });
   }
+
+  function updateEmployeeRole(){
+    db.findAllEmployees().then(([data])=>{
+      let empChoices = data.map(({id, first_name, last_name})=>({
+        name:`${first_name} ${last_name}`,
+        value: id
+      }))
+
+      inquirer.prompt([{
+        type: 'list',
+        name: 'employeeId', 
+        message:'which employee are you updating?',
+        choices: empChoices
+      }]).then((res)=>{
+        let employeId = res.employeId
+
+        db.findAllRoles().then(([rows])=>{
+        let roleChoices = rows.map(({id, title})=>({
+          name: title,
+          value: id
+        }))
+
+        inquirer.prompt([{
+          type: 'list',
+          name: 'roleId',
+          message:'Which role do you want to give to the Employee?',
+          choices: roleChoices
+        }]).then((res)=>{
+          db.updateEmployeesRoles(employeId, res.roleId)
+        }).then(()=> start())
+
+        })
+
+      })
+    })
+
+  }
+function addDepartments() {
+  inquirer.prompt([{
+    type: 'list',
+    name: 'addDepartment',
+    message:'What department would you like to add',
+    choices: roleChoices
+  }]).then((res)=>{
+    db.addDepartments(department)
+  }).then(()=> start())
+  
+}
+
+   
 
 
 start();
